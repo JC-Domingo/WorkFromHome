6588b9bef605c764ca41bc523c387723eafd0da0
JC-Domingo <jc.praxxys@gmail.com>
2020-07-15 17:38:07 +0800

    Updated Commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Blog Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+    <!-- Custom styles for this template -->
+    <link href="/css/app.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    @include ('layouts.nav')
+
+    <div class="blog-header">
+      <div class="container">
+        <h1 class="blog-title">The Bootstrap Blog</h1>
+        <p class="lead blog-description">An example blog template built with Bootstrap.</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        @yield('content')
+
+        @include ('layouts.sidebar')
+
+      </div>
+    </div>
+
+    @include ('layouts.footer')
+  </body>
+</html>
